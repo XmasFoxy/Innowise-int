e7e739aba7591f080eeea2d1f78f8f517cdb9a43
XmasFoxy <fintuk.olenevodsky@gmail.com>
2026-08-05 11:35:50 +0300

    feat: added functional of saved books

--- src/js/renderBooks.js
+++ src/js/renderBooks.js
@@ -1,6 +1,10 @@
+import { getDataFromStorage, setFavoriteBook,  isBookFavorite, addFavorite, removeFavorite } from "./localStorage.js";
+import { renderFavorites } from "./renderFavorites.js";
+
 function renderBooks(booksArr) {
   const gridShelf = document.querySelector('.main__shelf_block');
   booksArr.forEach((item) => {
+    //create elements
     const book = document.createElement('div');
     const coverContainer = document.createElement('div');
     const cover = document.createElement('img');
@@ -11,8 +15,11 @@ function renderBooks(booksArr) {
     const authorContainer = document.createElement('div');
     const author = document.createElement('p');
     const heartContainer = document.createElement('div');
-    const heart = document.createElement('img');
+    const svgNS = "http://www.w3.org/2000/svg";
+    const heart = document.createElementNS(svgNS, "svg");
+    const path = document.createElementNS(svgNS, "path");
 
+    //add attributes
     book.className = 'main__shelf_book';
     cover.src = item.coverURL;
     cover.alt = `A cover of the book by ${item.author}`;
@@ -27,17 +34,58 @@ function renderBooks(booksArr) {
     author.className = 'main__shelf_book_author';
     author.textContent = item.author;
     heartContainer.className = 'main-heart-container';
-    heart.src = 'assets/heart-outline.svg';
-    heart.alt = 'a heart icon for choosing a favorite song';
-    heart.className = 'main-heart';
+    heart.setAttribute("viewBox", "0 0 24 24");
+    heart.setAttribute("width", "25");
+    heart.setAttribute("height", "25");
+    heart.classList.add('main-heart');
+    path.setAttribute("d", "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z");
+    path.setAttribute("fill", "none");
+    path.setAttribute("stroke", "#333");
+    path.setAttribute("stroke-width", "2");
 
+    //render favorite/not favorite books
+    const bookId = `${item.title}_${item.author}`;
+    const bookData = {
+      id: bookId,
+      title: item.title,
+      author: item.author,
+      coverURL: item.coverURL,
+      data: item.data
+    }
+
+    if (isBookFavorite(bookId)) {
+      heart.classList.add('filled'); //fill heart with CSS
+    }
+
+    heartContainer.addEventListener('click', () => {
+      let favorites = getDataFromStorage();
+      const bookIndex = favorites.findIndex(fav => fav.id === bookId);
+
+      //correct localStorage
+      if (bookIndex === -1) {
+        favorites.push(bookData);
+      }
+      else {
+        favorites = favorites.filter(fav => fav.id !== bookId);
+      }
+      setFavoriteBook(favorites);
+
+      heart.classList.toggle('filled'); //change class of the book
+
+      renderFavorites(); // update list of the favorite books
+    });
+
+
+    //append elements
     coverContainer.append(cover);
     titleContainer.append(title);
     authorContainer.append(author);
     yearContainer.append(year);
+    heart.appendChild(path);
     heartContainer.append(heart);
     book.append(coverContainer, heartContainer, titleContainer, authorContainer, yearContainer);
     gridShelf.append(book);
+    
   });
 }
 

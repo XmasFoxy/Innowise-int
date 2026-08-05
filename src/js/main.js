d531b608ada8067634a89f9f76db3c63b3dfa59f
XmasFoxy <fintuk.olenevodsky@gmail.com>
2026-08-05 11:34:56 +0300

    feat: added functional of saved books

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import getBooks from "./API";
 import renderBooks from "./renderBooks";
+import { renderFavorites } from "./renderFavorites";
 
 async function bookManager() {
   try {
     const books = await getBooks();
     renderBooks(books);
+    renderFavorites();
   }
   catch(error) {
     console.error('Books not rendered', error);
